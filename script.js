f4ae20fcd7bd7c5874aeb8ae4e0cc45234ad9958
Nick Haglind <nhaglind@gmail.com>
2018-01-10 14:46:13 -0600

    add reset button

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 let squareLimit = 256;
 
-for (var i = 0; i < squareLimit; i++) {
-  let box = document.querySelector('#box');
-  let square = document.createElement('div');
-  square.className = 'square';
-  box.appendChild(square);
+function canvas() {
+  for (var i = 0; i < squareLimit; i++) {
+    let box = document.querySelector('#box');
+    let square = document.createElement('div');
+    square.className = 'square';
+    box.appendChild(square);
+  }
+  draw();
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseover', draw));
+canvas();
 
 function draw() {
-  this.style.backgroundColor = '#FFB6C1';
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.addEventListener('mouseover', pink));
+
+  function pink() {
+    this.style.backgroundColor = '#FFB6C1';
+  }
 }
+
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', reset);
+
+function reset() {
+  let box = document.querySelector('#box');
+  while (box.firstChild) {
+    box.removeChild(box.firstChild);
+  }
+  canvas();
+}
+
